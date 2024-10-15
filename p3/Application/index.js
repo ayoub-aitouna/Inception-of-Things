4e22ad6523c5c3a980aa40e065c43de30fff53a5
jrayoub <aitounaayoub05@gmail.com>
2024-10-15 19:08:51 +0100

    changed p3 to use my app instead

--- p3/Application/index.js
+++ p3/Application/index.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 8888
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World FROM V2 !')
 })
 
 app.listen(port, () => {
-  console.log(`aaitouna app listening on port ${port}`)
+  console.log(`app listening on port ${port}`)
 })
